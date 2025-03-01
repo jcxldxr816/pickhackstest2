f7108a4f240587c2e979d301c7bda5f685607673
James Calder <154292961+jcxldxr816@users.noreply.github.com>
2025-03-01 09:51:40 -0600

    basic card class

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public abstract class Card
+{
+    public string Name { get; private set; }
+    public enum CardType { Offense, Support, Modifier }
+    public CardType Type { get; protected set; }
+
+    public Card(string name, CardType type)
+    {
+        Name = name;
+        Type = type;
+    }
+}
+
+public class OffenseCard : Card
+{
+    public int Damage { get; private set; }
+    public int Health { get; private set; }
+
+    public OffenseCard(string name, int damage, int health) 
+        : base(name, CardType.Offense)
+    {
+        Damage = damage;
+        Health = health;
+    }
+}
+
+public class SupportCard : Card
+{
+    public string Behavior { get; private set; }
+
+    public SupportCard(string name, string behavior) 
+        : base(name, CardType.Support)
+    {
+        Behavior = behavior;
+    }
+}
+
+public class ModifierCard : Card
+{
+    public string Behavior { get; private set; }
+
+    public ModifierCard(string name, string behavior) 
+        : base(name, CardType.Modifier)
+    {
+        Behavior = behavior;
+    }
+}
--- Assets/Scripts/cardGenerator.cs
+++ Assets/Scripts/cardGenerator.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class cardGenerator
+public class CardGenerator : MonoBehaviour
 {
+    private const int OFFENSE_DAMAGE_MAX = 10;
+    private const int OFFENSE_HEALTH_MAX = 10;
+    private const int CARDS_DEALT = 5; //per hand
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
     
 }
